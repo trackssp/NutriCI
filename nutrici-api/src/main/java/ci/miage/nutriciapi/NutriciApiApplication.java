7b3a27f4397547b5adc314fd93fd7a489dfafea5
Kple David <Dtakple@gmail.com>
2026-07-24 02:24:33 +0000

    Initialisation du projet Spring Boot NutriCI

---
+++ nutrici-api/src/main/java/ci/miage/nutriciapi/NutriciApiApplication.java
@@ -0,0 +1,13 @@
+package ci.miage.nutriciapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriciApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriciApiApplication.class, args);
+	}
+
+}
---
+++ nutrici-api/src/test/java/ci/miage/nutriciapi/NutriciApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.miage.nutriciapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriciApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
